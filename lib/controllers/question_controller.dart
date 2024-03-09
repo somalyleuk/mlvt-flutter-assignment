58b0dd49a638ad439770d878d298fbc6a64d6ece
Leuk Somaly E2 <malyonly88@gmail.com>
2024-03-09 23:24:49 +0700

    update

--- lib/controllers/question_controller.dart
+++ lib/controllers/question_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:testapp/service/api_service.dart';
+
 import 'package:testapp/models/faqs_model/questions_model.dart';
 
 class QuestionController extends GetxController {
@@ -17,7 +18,7 @@ class QuestionController extends GetxController {
       questions.assignAll(data['questions']
           .map<QuestionModel>((question) => QuestionModel.fromJson(question)));
     } catch (e) {
-      print('Error fetching data: $e');
+      print('Error fetching data: $e ');
     }
   }
 }
